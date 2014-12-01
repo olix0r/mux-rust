39a9178ddaa8d8dc550857fc07bc220ee1c722e8
Oliver Gould <ver@twitter.com>
2014-12-01 17:42:24 +0000

    test fails everything is good

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,78 @@
 /// See: https://github.com/twitter/finagle/blob/master/finagle-mux/src/main/scala/com/twitter/finagle/mux/package.scala
 pub mod mux {
 
-    // TODO
-    // It's super lazy and inefficient to be using Vectors here (over, i.e. array slices),
-    // but I'm not yet emotionally prepared to manage lifetimes, and ownership and what have you.
-    // That'll come next.
+    // TODO these should be moved into other crates...
+    pub mod misc {
 
-    pub struct Path<'t>(&'t [&'t str]);
+        /// A simple wrapper type for an array of bytes.
+        pub struct Buf<'t>(&'t [u8]);
+        static EMPTY_BUF: Buf<'static> = Buf(&[]);
+        impl<'t> Buf<'t> {
+            pub fn empty() -> Buf<'t> { EMPTY_BUF }
 
-    pub struct Buf<'t>(&'t [u8]);
+            pub fn from_string(string: &'t str) -> Buf<'t> { Buf(string.as_bytes()) }
 
-    /* TODO
+            pub fn as_bytes(&'t self) -> &'t [u8] {
+                let Buf(bytes) = *self;
+                bytes
+            }
+        }
+
+        pub struct Path<'t>(pub &'t [Buf<'t>]);
+        static EMPTY_PATH: Path<'static> = Path(&[]);
+        impl<'t> Path<'t> {
+            pub fn empty() -> Path<'t> { EMPTY_PATH }
+
+            // TODO
+            pub fn read(str: &'t [str]) -> Path<'t> { EMPTY_PATH }
+        }
+
+        pub struct Dentry<'t>(pub &'t str, pub &'t str);
+        impl<'t> Dentry<'t> {
+            pub fn source(&self) -> &'t str {
+                let Dentry(source, _) = *self;
+                source
+            }
+
+            pub fn target(&self) -> &'t str {
+                let Dentry(_, target) = *self;
+                target
+            }
+        }
+
+        pub struct Dtab<'t>(pub &'t [Dentry<'t>]);
+        static EMPTY_DTAB: Dtab<'static> = Dtab(&[]);
+        impl<'t> Dtab<'t> {
+            pub fn empty() -> Dtab<'t> { EMPTY_DTAB }
+
+            pub fn dentries(&self) -> &'t [Dentry<'t>] {
+                let Dtab(dentries) = *self;
+                dentries
+            }
+        }
+
+        pub struct Context<'t>(pub Buf<'t>, pub Buf<'t>);
+        impl<'t> Context<'t> {
+            pub fn key(&self) -> Buf<'t> {
+                let Context(k, _) = *self;
+                k
+            }
+
+            pub fn val(&self) -> Buf<'t> {
+                let Context(_, v) = *self;
+                v
+            }
+        }
+
+        pub struct Contexts<'t>(pub &'t [Context<'t>]);
+        static EMPTY_CONTEXTS: Contexts<'static> = Contexts(&[]);
+        impl<'t> Contexts<'t> {
+            pub fn empty() -> Contexts<'t> { EMPTY_CONTEXTS }
+        }
+    }
+
+    /*
+     * TODO
 
     pub struct Request {
         destination: Path,
@@ -23,109 +85,99 @@ pub mod mux {
     }
      */
 
-    pub struct Dentry<'t>(&'t str, &'t str);
-    pub struct Dtab<'t>(&'t [Dentry<'t>]);
+    /// Mux protocol support for de/serializing mux messages.
+    pub mod proto {
+        use super::misc::{Buf, Context, Contexts, Path, Dentry, Dtab};
 
-    pub struct Tag(u8, u8, u8);
+        pub struct Tag(u8, u8, u8);
 
-    pub struct Context<'t>(Buf<'t>, Buf<'t>);
-    pub struct Contexts<'t>(&'t [Context<'t>]);
+        pub enum Message<'t> {
+            // Treq(Tag, Option<TraceId>, Buf),
+            // RreqOk(Tag, Buf),
+            // RreqError(Tag, str),
+            // RreqNack(Tag),
 
-    /*
-    pub mod messages {
-        pub struct Tdispatch(Tag, Contexts, Path, Dtab, Buf);
-        pub struct RdispatchOk(Tag, Contexts, Buf);
-        pub struct RdispatchErr(Tag, Contexts, String);
-        pub struct RdispatchNack(Tag, Contexts);
+            Tdispatch(Tag, Contexts<'t>, Path<'t>, Dtab<'t>, Buf<'t>),
+            RdispatchOk(Tag, Contexts<'t>, Buf<'t>),
+            RdispatchError(Tag, Contexts<'t>, &'t str),
+            RdispatchNack(Tag, Contexts<'t>),
 
-        pub struct Tdrain(Tag);
-        pub struct Rdrain(Tag);
+            Tdrain(Tag),
+            Rdrain(Tag),
 
-        pub struct Tping(Tag);
-        pub struct Rping(Tag);
+            Tping(Tag),
+            Rping(Tag),
+            Tdiscarded(Tag, &'t str),
+            Tlease(u8, u64),
 
-        pub struct Tdiscarded(Tag, String);
-        pub struct Tlease(u8, u64);
+            Rerr(Tag, &'t str),
+        }
 
-        pub struct Rerr(Tag, String);
-    }
-     */
+        pub enum DecodedMessage<'t> {
+            Decoded(Message<'t>),
+            TooShort(uint),
+            UnknownType(i8),
+        }
 
-    pub enum Message<'t> {
-        // Treq(Tag, Option<TraceId>, Buf),
-        // RreqOk(Tag, Buf),
-        // RreqError(Tag, str),
-        // RreqNack(Tag),
+        impl<'t> Message<'t> {
 
-        Tdispatch(Tag, Contexts<'t>, Path<'t>, Dtab<'t>, Buf<'t>),
-        RdispatchOk(Tag, Contexts<'t>, Buf<'t>),
-        RdispatchError(Tag, Contexts<'t>, &'t str),
-        RdispatchNack(Tag, Contexts<'t>),
+            /// Decode a slice of bytes as a mux message.
+            pub fn decode(bytes: &'t [u8]) -> DecodedMessage {
+                if bytes.len() < 4 {
+                    TooShort(bytes.len())
+                } else {
+                    let tag_slice = bytes.slice(1, 4);
+                    let tag = Tag(tag_slice[0], tag_slice[1], tag_slice[2]);
+                    let rest = bytes.slice_from(4);
+                    match bytes[0] as i8 {
+                        // 1 => Some(decodeTreq(tag, rest)),
+                        //-1 => Some(decodeRreq(tag, rest)),
+                        kind => UnknownType(kind),
+                    }
+                }
+            }
 
-        Tdrain(Tag),
-        Rdrain(Tag),
+            /// Encode a mux message as bytes.
+            pub fn encode(&self) -> Vec<u8> {
+                match *self {
+                    Tdispatch(tag, Contexts(contexts), path, dtab, buf) => {
+                        let csz = (*contexts).iter().fold(0u, |sum, context| -> uint {
+                            let Context(kbuf, vbuf) = *context;
+                            sum + kbuf.as_bytes().len() + vbuf.as_bytes().len()
+                        });
+                        Vec::with_capacity(csz)
+                    },
 
-        Tping(Tag),
-        Rping(Tag),
-        Tdiscarded(Tag, &'t str),
-        Tlease(u8, u64),
-
-        Rerr(Tag, &'t str),
-    }
-
-    pub enum DecodedMessage<'t> {
-        Ok(Message<'t>),
-        TooShort(uint),
-        UnknownType(i8),
-    }
-
-    impl<'t> Message<'t> {
-        pub fn decode(bytes: &'t [u8]) -> DecodedMessage {
-            if bytes.len() < 4 {
-                TooShort(bytes.len())
-            } else {
-                let tag_slice = bytes.slice(1, 4);
-                let tag = Tag(tag_slice[0], tag_slice[1], tag_slice[2]);
-                let rest = bytes.slice_from(4);
-                match bytes[0] as i8 {
-                    // 1 => Some(decodeTreq(tag, rest)),
-                    //-1 => Some(decodeRreq(tag, rest)),
-                    kind => UnknownType(kind),
+                    _ => Vec::new(),
                 }
             }
         }
 
-        pub fn encode(message: Message) -> Vec<u8> {
-            match message {
-                Tdispatch(tag, Contexts(contexts), path, dtab, buf) => {
-                    let csz = (*contexts).iter().fold(0u, |sum, context| -> uint {
-                        let Context(Buf(a), Buf(b)) = *context;
-                        sum + a.len() + b.len()
-                    });
-                    Vec::with_capacity(csz)
-                },
+        #[cfg(test)]
+        mod test {
+            use super::super::misc::{Contexts, Path, Dtab, Buf};
+            use super::{Tag, Tdispatch, Message, Decoded};
 
-                _ => Vec::new(),
+            fn mk_tdispatch<'t>() -> Message<'t> {
+                let tag = Tag(0,0,1);
+                let contexts = Contexts::empty();
+                let path = Path::empty();
+                let dtab = Dtab::empty();
+                let body = Buf::empty();
+                Tdispatch(tag, contexts, path, dtab, body)
             }
-        }
-    }
 
-    #[cfg(test)]
-    mod test {
-        use super::{Tag, Contexts, Path, Dtab, Buf, Tdispatch, Message};
+            #[test]
+            fn tdispatch() {
+                let orig = mk_tdispatch();
+                let bytes = orig.encode();
 
-        #[test]
-        fn encode_tdispatch() {
-            let tag = Tag(0,0,1);
-            let contexts = Contexts(&[]);
+                match Message::decode(bytes.as_slice()) {
+                    Decoded(Tdispatch(tag, contexts, path, dtab, body)) => (),
 
-            let p0 = ["foo", "bar", "bah"];
-            let path = Path(&p0);
-
-            let dtab = Dtab(&[]);
-            let body = Buf(&[]);
-            let tdispatch = Tdispatch(tag, contexts, path, dtab, body);
-            Message::encode(tdispatch);
+                    _ => fail!("could not decode a Tdispatch")
+                }
+            }
         }
     }
 }
