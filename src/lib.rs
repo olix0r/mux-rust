44bf05a24513d63207bef5ed4b5d20af19b537b1
Oliver Gould <ver@twitter.com>
2014-12-02 17:03:27 +0000

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,51 @@
 /// An implementation of mux in Rust.
 /// See: https://github.com/twitter/finagle/blob/master/finagle-mux/src/main/scala/com/twitter/finagle/mux/package.scala
 pub mod mux {
-
     // TODO these should be moved into other crates...
     pub mod misc {
+        use std::fmt;
+        use std::str;
 
         /// A simple wrapper type for an array of bytes.
+        #[deriving(PartialEq,Eq)]
         pub struct Buf<'t>(&'t [u8]);
         static EMPTY_BUF: Buf<'static> = Buf(&[]);
         impl<'t> Buf<'t> {
+            #[inline]
             pub fn empty() -> Buf<'t> { EMPTY_BUF }
-
-            pub fn from_string(string: &'t str) -> Buf<'t> { Buf(string.as_bytes()) }
 
             pub fn as_bytes(&'t self) -> &'t [u8] {
                 let Buf(bytes) = *self;
                 bytes
             }
+
+            pub fn as_str(&'t self) -> Option<&'t str> {
+                let Buf(bytes) = *self;
+                str::from_utf8(bytes)
+            }
+
+            pub fn from_str<'u>(s: &'u str) -> Option<Buf<'u>> { Some(Buf(s.as_bytes())) }
         }
 
+        impl<'t> fmt::Show for Buf<'t> {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                let Buf(bytes) = *self;
+                write!(f, "{}", str::from_utf8(bytes))
+            }
+        }
+
+        #[deriving(PartialEq,Eq,Show)]
         pub struct Path<'t>(pub &'t [Buf<'t>]);
         static EMPTY_PATH: Path<'static> = Path(&[]);
         impl<'t> Path<'t> {
+            #[inline]
             pub fn empty() -> Path<'t> { EMPTY_PATH }
 
             // TODO
-            pub fn read(str: &'t [str]) -> Path<'t> { EMPTY_PATH }
+            pub fn from_str<'u>(_: &'u [str]) -> Path<'u> { EMPTY_PATH }
         }
 
+        #[deriving(PartialEq,Eq,Show)]
         pub struct Dentry<'t>(pub &'t str, pub &'t str);
         impl<'t> Dentry<'t> {
             pub fn source(&self) -> &'t str {
@@ -41,9 +59,11 @@ pub mod mux {
             }
         }
 
+        #[deriving(PartialEq,Eq,Show)]
         pub struct Dtab<'t>(pub &'t [Dentry<'t>]);
         static EMPTY_DTAB: Dtab<'static> = Dtab(&[]);
         impl<'t> Dtab<'t> {
+            #[inline]
             pub fn empty() -> Dtab<'t> { EMPTY_DTAB }
 
             pub fn dentries(&self) -> &'t [Dentry<'t>] {
@@ -52,6 +72,7 @@ pub mod mux {
             }
         }
 
+        #[deriving(PartialEq,Eq,Show)]
         pub struct Context<'t>(pub Buf<'t>, pub Buf<'t>);
         impl<'t> Context<'t> {
             pub fn key(&self) -> Buf<'t> {
@@ -65,9 +86,11 @@ pub mod mux {
             }
         }
 
+        #[deriving(PartialEq,Eq,Show)]
         pub struct Contexts<'t>(pub &'t [Context<'t>]);
         static EMPTY_CONTEXTS: Contexts<'static> = Contexts(&[]);
         impl<'t> Contexts<'t> {
+            #[inline]
             pub fn empty() -> Contexts<'t> { EMPTY_CONTEXTS }
         }
     }
@@ -87,15 +110,18 @@ pub mod mux {
 
     /// Mux protocol support for de/serializing mux messages.
     pub mod proto {
-        use super::misc::{Buf, Context, Contexts, Path, Dentry, Dtab};
+        use std::io::{IoResult, IoError, Reader, BufReader};
+        use super::misc::{Buf, Context, Contexts, Path, Dtab};
 
-        pub struct Tag(u8, u8, u8);
+        #[deriving(PartialEq,Eq,Show)]
+        pub struct Tag(u64);
 
+        #[deriving(PartialEq,Eq,Show)]
         pub enum Message<'t> {
             // Treq(Tag, Option<TraceId>, Buf),
             // RreqOk(Tag, Buf),
             // RreqError(Tag, str),
-            // RreqNack(Tag),
+            //RreqNack(Tag),
 
             Tdispatch(Tag, Contexts<'t>, Path<'t>, Dtab<'t>, Buf<'t>),
             RdispatchOk(Tag, Contexts<'t>, Buf<'t>),
@@ -113,53 +139,94 @@ pub mod mux {
             Rerr(Tag, &'t str),
         }
 
-        pub enum DecodedMessage<'t> {
-            Decoded(Message<'t>),
-            TooShort(uint),
+        trait MessageReader<'t> {
+            fn read_type(&mut self) -> IoResult<i8>;
+            fn read_tag(&mut self) -> IoResult<Tag>;
+            fn read_contexts(&mut self) -> IoResult<Contexts<'t>>;
+
+            //fn read_message(&mut self) -> IoResult<Message<'t>, IoError>
+        }
+
+        impl<'t, R: Reader> MessageReader<'t> for R {
+            fn read_type(&mut self) -> IoResult<i8> { self.read_i8() }
+
+            fn read_tag(&mut self) -> IoResult<Tag> {
+                self.read_be_uint_n(3).map(|n| -> Tag { Tag(n) })
+            }
+
+            fn read_contexts(&mut self) -> IoResult<Contexts<'t>> {
+                match self.read_be_u16() {
+                    Err(io) => Err(io),
+
+                    Ok(n) if n == 0 =>
+                        Ok(Contexts::empty()),
+
+                    Ok(_n) => {
+                        let n = _n as uint;
+                        let mut contexts: Vec<Context> = Vec::with_capacity(n);
+                        for i in range(0, n) {
+                            contexts.insert(i, Context(Buf::empty(), Buf::empty()));
+                        }
+                        Ok(Contexts(contexts.as_slice().clone()))
+                    }
+                }
+            }
+        }
+
+        pub enum DecodeErr {
+            IoErr(IoError),
             UnknownType(i8),
         }
 
         impl<'t> Message<'t> {
 
             /// Decode a slice of bytes as a mux message.
-            pub fn decode(bytes: &'t [u8]) -> DecodedMessage {
-                if bytes.len() < 4 {
-                    TooShort(bytes.len())
-                } else {
-                    let tag_slice = bytes.slice(1, 4);
-                    let tag = Tag(tag_slice[0], tag_slice[1], tag_slice[2]);
-                    let rest = bytes.slice_from(4);
-                    match bytes[0] as i8 {
-                        // 1 => Some(decodeTreq(tag, rest)),
-                        //-1 => Some(decodeRreq(tag, rest)),
-                        kind => UnknownType(kind),
-                    }
+            pub fn decode<'u>(bytes: &'u [u8]) -> Result<Message<'u>, DecodeErr> {
+                let mut reader = BufReader::new(bytes);
+
+                match reader.read_type() {
+                    // Ok( 1) => Some(decodeTreq(tag, rest)),
+                    // Ok(-1) => Some(decodeRreq(tag, rest)),
+
+                    // Ok( 2) => decodeTdispatch(tag, rest),
+                    // Ok(-2) => decodeRdispatch(tag, rest),
+
+                    Ok(kind) => Err(UnknownType(kind)),
+                    Err(ioe) => Err(IoErr(ioe)),
                 }
             }
+
+            // fn decode_tdispatch<'u>(tag: Tag, bytes: &'u [u8]) -> Decoded<'u, Message<'u>> {
+            //     decodeContexts
+            // }
 
             /// Encode a mux message as bytes.
             pub fn encode(&self) -> Vec<u8> {
                 match *self {
-                    Tdispatch(tag, Contexts(contexts), path, dtab, buf) => {
-                        let csz = (*contexts).iter().fold(0u, |sum, context| -> uint {
-                            let Context(kbuf, vbuf) = *context;
-                            sum + kbuf.as_bytes().len() + vbuf.as_bytes().len()
-                        });
-                        Vec::with_capacity(csz)
+                    Tdispatch(tag, Contexts(contexts), Path(path), dtab, buf) => {
+                        let tagsz = 3;
+                        let ctxsz = Message::context_sz(contexts);
+                        Vec::with_capacity(tagsz + ctxsz)
                     },
 
                     _ => Vec::new(),
                 }
             }
+
+            fn context_sz<'u>(contexts: &'u [Context]) -> uint {
+                (*contexts).iter().fold(0u, |sum, &Context(kbuf, vbuf)| -> uint {
+                    sum + kbuf.as_bytes().len() + vbuf.as_bytes().len()
+                })
+            }
         }
 
         #[cfg(test)]
         mod test {
-            use super::super::misc::{Contexts, Path, Dtab, Buf};
-            use super::{Tag, Tdispatch, Message, Decoded};
+            use mux::misc::{Contexts, Path, Dtab, Buf};
+            use super::{Tag, Tdispatch, Message, IoErr, UnknownType};
 
             fn mk_tdispatch<'t>() -> Message<'t> {
-                let tag = Tag(0,0,1);
+                let tag = Tag(1u as u64);
                 let contexts = Contexts::empty();
                 let path = Path::empty();
                 let dtab = Dtab::empty();
@@ -173,9 +240,17 @@ pub mod mux {
                 let bytes = orig.encode();
 
                 match Message::decode(bytes.as_slice()) {
-                    Decoded(Tdispatch(tag, contexts, path, dtab, body)) => (),
+                    Ok(decoded@Tdispatch(_,_,_,_,_)) =>
+                        assert_eq!(orig, decoded),
 
-                    _ => fail!("could not decode a Tdispatch")
+                    Ok(other) =>
+                        fail!("decoded unexpected message type: {}", other),
+
+                    Err(IoErr(ioe)) =>
+                        fail!("I/O error: {}", ioe),
+
+                    Err(UnknownType(typ)) =>
+                        fail!("unknown message type: {}", typ),
                 }
             }
         }
