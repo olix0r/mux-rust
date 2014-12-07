4d14b847ddb281500d90262ca0c1c6e953389b98
Oliver Gould <ver@twitter.com>
2014-12-07 01:54:48 +0000

    Add encoding benchmark

--- benches/tdispatch.rs
+++ benches/tdispatch.rs
@@ -8,60 +8,58 @@ use mux::writer::MessageWriter;
 use std::io::{BufReader, MemWriter};
 use test::Bencher;
 
-
-static TDISPATCH_BUF: &'static [u8] = [
-    types::TDISPATCH as u8,
-    0, 1, 2, // tag
-
-    // contexts:
-    0, 2, // 2 contexts
-
-    // context 0 key
-    0, 4, // length
-    1, 2, 3, 4,
-
-    // context 0 val
-    0, 2, // length
-    6, 7,
-
-    // context 1 key
-    0, 2, // length
-    3, 4,
-
-    // context 1 val
-    0, 3, // length
-    6, 7, 8,
-
-    // dst
-    0, 4, // length
-    '/' as u8, 66, 65, 68, // "/BAD"
-
-    // dtab: /BAD => /DAD
-    0, 1, // length
-    0, 4, // source length
-    '/' as u8, 66, 65, 68, // "/BAD"
-    0, 4, // tree length
-    '/' as u8, 68, 65, 68, // "/DAD"
-
-    // data: [0 .. 20)
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
-
-
 #[inline]
-fn read_tdispatch() {
-    let mut reader = BufReader::new(TDISPATCH_BUF);
+fn read(buf: &[u8]) {
+    let mut reader = BufReader::new(buf;
     reader.read_message().ok();
 }
 
 #[inline]
-fn write_tdispatch(msg: &Message) {
+fn write(msg: &Message) {
     let mut writer = MemWriter::new();
     writer.write_message(msg).ok();
 }
 
 #[bench]
 fn bench_read_tdispatch(bench: &mut Bencher) {
-    bench.iter(|| read_tdispatch());
+    let buf = [
+        types::TDISPATCH as u8,
+        0, 1, 2, // tag
+
+        // contexts:
+        0, 2, // 2 contexts
+
+        // context 0 key
+        0, 4, // length
+        1, 2, 3, 4,
+
+        // context 0 val
+        0, 2, // length
+        6, 7,
+
+        // context 1 key
+        0, 2, // length
+        3, 4,
+
+        // context 1 val
+        0, 3, // length
+        6, 7, 8,
+
+        // dst
+        0, 4, // length
+        '/' as u8, 66, 65, 68, // "/BAD"
+
+        // dtab: /BAD => /DAD
+        0, 1, // length
+        0, 4, // source length
+        '/' as u8, 66, 65, 68, // "/BAD"
+        0, 4, // tree length
+        '/' as u8, 68, 65, 68, // "/DAD"
+
+        // data: [0 .. 20)
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+
+    bench.iter(|| read(buf));
     bench.bytes = TDISPATCH_BUF.len() as u64;
 }
 
@@ -74,6 +72,7 @@ fn bench_write_tdispatch(bench: &mut Bencher) {
         "/BAD".to_string(),
         Dtab(vec![Dentry::new("/BAD".to_string(), "/DAD".to_string())]),
         vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]);
+
     bench.iter(|| write_tdispatch(&msg));
     bench.bytes = TDISPATCH_BUF.len() as u64;
 }
